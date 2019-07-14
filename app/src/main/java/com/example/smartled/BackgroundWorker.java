d356169c80e1b38312c903504fb81e998d7d8b07
Prashanth Venkatesha <prashanth.venkatesha@arinko.de>
2019-07-14 23:11:59 +0200

    plugwise2 value read from db

--- app/src/main/java/com/example/smartled/BackgroundWorker.java
+++ app/src/main/java/com/example/smartled/BackgroundWorker.java
@@ -23,7 +23,7 @@ public class BackgroundWorker extends AsyncTask <String, Void, String> {
     String url, user, pass;
 
     ArrayList<Double> actual_power_1 = new ArrayList<Double>();
-    ArrayList<String> actual_power_2 = new ArrayList<String>();
+    ArrayList<Double> actual_power_2 = new ArrayList<Double>();
 
     public AsyncResponse delegate = null;
 
@@ -48,18 +48,20 @@ public class BackgroundWorker extends AsyncTask <String, Void, String> {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection(url, user, pass);
 
-            Statement st = con.createStatement();
-            ResultSet rs1 = st.executeQuery("select * from plugwise1");
-            //ResultSet rs2 = st.executeQuery("select * from plugwise2");
-            ResultSetMetaData rsmd1 = rs1.getMetaData();
+            Statement st1 = con.createStatement();
+            Statement st2 = con.createStatement();
+            ResultSet rs1 = st1.executeQuery("select * from plugwise1");
+            ResultSet rs2 = st2.executeQuery("select * from plugwise2");
+            ResultSetMetaData rsmd11 = rs1.getMetaData();
+            ResultSetMetaData rsmd12 = rs1.getMetaData();
 
             while(rs1.next()) {
                 actual_power_1.add(rs1.getDouble(3));
             }
 
-            /*while(rs2.next()) {
-                actual_power_2.add(rs1.getString(3));
-            }*/
+            while(rs2.next()) {
+                actual_power_2.add(rs2.getDouble(3));
+            }
         }
         catch (Exception e){
           return new String("Exeption:" + e.getMessage());
@@ -71,18 +73,30 @@ public class BackgroundWorker extends AsyncTask <String, Void, String> {
     @Override
     protected void onPostExecute(String result) {
 
-        String line = "Actual Power1: " + actual_power_1.get(actual_power_1.size() - 1) + "\n";
+        String line = "Actual Power1: " + actual_power_1.get(actual_power_1.size() - 1) + "\n" +
+                      "Actual Power2: " + actual_power_2.get(actual_power_2.size() - 1) + "\n" ;
         MainActivity.plugwiseData.setText(line);
-        Double last_val = actual_power_1.get(actual_power_1.size() - 1);
+        Double last_val1 = actual_power_1.get(actual_power_1.size() - 1);
+        Double last_val2 = actual_power_2.get(actual_power_2.size() - 1);
 
-        if(last_val < WARNING_THRESHOLD) {
+        if(last_val1 < WARNING_THRESHOLD) {
             //SocketAsyncTask led_off = new SocketAsyncTask();
             //led_off.execute("0");
             delegate.displayLed("green", 1);
-        } else if((last_val >= WARNING_THRESHOLD) && (last_val < SWITCH_OFF_THRESHOLD)){
+        } else if((last_val1 >= WARNING_THRESHOLD) && (last_val1 < SWITCH_OFF_THRESHOLD)){
             delegate.displayLed("red", 1);
         } else {
             delegate.displayLed("white", 1);
+        }
+
+        if(last_val2 < WARNING_THRESHOLD) {
+            //SocketAsyncTask led_off = new SocketAsyncTask();
+            //led_off.execute("0");
+            delegate.displayLed("green", 2);
+        } else if((last_val2 >= WARNING_THRESHOLD) && (last_val2 < SWITCH_OFF_THRESHOLD)){
+            delegate.displayLed("red", 2);
+        } else {
+            delegate.displayLed("white", 2);
         }
 
     }
---
+++ app/src/main/java/com/example/smartled/PowerRead.java
@@ -0,0 +1,113 @@
+package com.example.smartled;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.Socket;
+import java.util.Properties;
+
+public class PowerRead extends AsyncTask<String, Void, Void> {
+    Socket socket;
+
+    @Override
+    protected Void doInBackground(String... strings) {
+        int option = Integer.valueOf(strings[0]);
+        /*try {
+            InetAddress inetAddress = InetAddress.getByName(MainActivity.wifiModuleIp);
+            socket = new java.net.Socket(inetAddress, MainActivity.wifiModulePort);
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            dataOutputStream.writeBytes(led_switch);
+            dataOutputStream.close();
+            socket.close();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        try {
+            String g=executeRemoteCommand("pi", "smart@team5", "192.168.0.114", 22, option);
+            //Log.d("DDD",g);
+            MainActivity.plugwiseData.setText(option+g);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String executeRemoteCommand(
+            String username,
+            String password,
+            String hostname,
+            int port,
+            int option) throws Exception {
+        String command=null;
+        switch (option)
+        {
+            case 1:command="mosquitto_sub -t plugwise2py/state/power/000D6F0005690BAB -C 1";
+                break;
+            case 2:command="mosquitto_sub -t plugwise2py/state/power/000D6F0003562BE1 -C 1";
+                break;
+        }
+        String g = null;
+        try {
+            JSch jsch = new JSch();
+            Session session = jsch.getSession(username, hostname, 22);
+            session.setPassword(password);
+            Log.d("SSH", "session done");
+
+            // Avoid asking for key confirmation
+            Properties prop = new Properties();
+            prop.put("StrictHostKeyChecking", "no");
+            session.setConfig(prop);
+            session.connect();
+            Log.d("SSH", "session conn");
+
+            // SSH Channel
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            Log.d("SSH", "BAOS done");
+            Channel channel = session.openChannel("exec");
+            ((ChannelExec) channel).setCommand(command);
+            Log.d("SSH", "chsnnel done");
+            channel.setInputStream(null);
+            ((ChannelExec) channel).setErrStream(System.err);
+            InputStream in = channel.getInputStream();
+            channel.connect();
+
+            byte[] tmp = new byte[1024];
+            while (true) {
+                while (in.available() > 0) {
+                    int i = in.read(tmp, 0, 1024);
+                    if (i < 0) break;
+                    g = new String(tmp, 0, i);
+                }
+                if (channel.isClosed()) {
+                    if (in.available() > 0) continue;
+                    System.out.println("exit-status: " + channel.getExitStatus());
+                    break;
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception ee) {
+                }
+            }
+
+            channel.disconnect();
+            session.disconnect();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        return g;
+
+    }
+
+
+
+}
